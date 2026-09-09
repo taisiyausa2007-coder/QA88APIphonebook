6e9a5b75e41859c27274813c6832b82d86e9867a
Taisiya <Tayausa2007@yahoo.com>
2026-09-09 08:42:54 -0400

    Start project 1

--- utils/conftest.py
+++ utils/conftest.py
@@ -1,15 +1,37 @@
 import pytest
 import requests
+import random
+import time
 from config import *
-@pytest.fixture(scope="session")
+from faker import Faker
+fake = Faker()
+
+from models.user_dto import User
+
+
+
 @pytest.fixture(scope="session")
 def registration_url():
-    return BASE_URL + API_VERSION + REGISTRATION
+    return BASE_URL + API_VERSION + REGISTRATION_URL
+
 @pytest.fixture(scope="session")
 def login_url():
     return BASE_URL + API_VERSION + LOGIN_URL
+
 @pytest.fixture(scope="session")
 def session():
     s= requests.Session()
     yield s
     s.close()
+
+@pytest.fixture(scope="function")
+def random_user():
+    username = f"qa_{int(time.time())}_{fake.email()}"
+    password = fake.password(
+        length=random.randint(8, 15),
+        special_chars=True,
+        digits=True,
+        upper_case=True,
+        lower_case=True,
+    )
+    return User(username=username, password=password)
