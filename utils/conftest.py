80fd632b8e2916075ce6ad42c2b405a300f8903c
Taisiya <Tayausa2007@yahoo.com>
2026-09-09 07:42:03 -0400

    Start project

---
+++ utils/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+import requests
+from config import *
+@pytest.fixture(scope="session")
+@pytest.fixture(scope="session")
+def registration_url():
+    return BASE_URL + API_VERSION + REGISTRATION
+@pytest.fixture(scope="session")
+def login_url():
+    return BASE_URL + API_VERSION + LOGIN_URL
+@pytest.fixture(scope="session")
+def session():
+    s= requests.Session()
+    yield s
+    s.close()
